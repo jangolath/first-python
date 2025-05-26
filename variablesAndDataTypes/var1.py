5b305cffe72b8a33c1e08d41c64521f80e6813a6
Jasom Wood <jwoodtechnologies@gmail.com>
2025-05-26 15:21:21 -0500

    finished numbers and string manipulation introduction

--- variablesAndDataTypes/var1.py
+++ variablesAndDataTypes/var1.py
@@ -1,8 +1,36 @@
+from math import *
+
+character_name = "John"
+character_age = 70
+is_male = True
+
+print(f"There once was a man named\n {character_name}, ".lower())
+print(f"he was {character_age} years old.")
+
 
 character_name = "George"
-character_age = 70
-print("There once was a man named George, ")
-print("he was 70 years old.")
-print("He really liked the name George, ")
-print("but didn't like being 70.")
+character_age = 68
+
+print(f"He really liked the name {character_name}, ")
+print(f"but didn't like being {character_age}.")
 # This is a simple Python script that tells a short story
+
+# This is just playing around with Python's string utility and manipulations
+phrase = "Giraffe Academy"
+print(len(phrase))
+print(phrase.isupper())
+print(phrase[2])
+
+# playing around with Python's number processing
+my_num = (5 % 15)
+
+print(str(my_num) + " is my favorite number.")
+
+my_power_num = pow(10, 2)
+print (my_power_num)
+print(max(10,6))
+print(min(10,6))
+print(round(3,7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
