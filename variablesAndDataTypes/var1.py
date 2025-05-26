55cb1154d1f719a8ebca9475fa547a6061cce867
Jason Wood <jwoodtechnologies@gmail.com>
2025-05-26 11:36:20 -0500

    just following a simple tutorial. Going really smoothly with concepts. FastAPI, SQLAlchemy, PostGress, Asyncio will need reviewed

---
+++ variablesAndDataTypes/var1.py
@@ -0,0 +1,8 @@
+
+character_name = "George"
+character_age = 70
+print("There once was a man named George, ")
+print("he was 70 years old.")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
+# This is a simple Python script that tells a short story
