dec44b91d920744309ce895419a2d5d854d93d68
Jasom Wood <jwoodtechnologies@gmail.com>
2025-05-26 15:37:53 -0500

    calculator made

---
+++ calculator/calculator1.py
@@ -0,0 +1,5 @@
+num1 = input("Enter a Number: ")      # by default python makes all inputs a string, so must convert user's input to int.
+num2 = input("Enter next number: ")
+
+result = float(num1) + float(num2)
+print (f"Added Result = {result}")
