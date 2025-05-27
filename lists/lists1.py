b76529c53e264cfc0265a1eeacc546835441f830
Jasom Wood <jwoodtechnologies@gmail.com>
2025-05-26 19:49:34 -0500

    mad-lib stash been working on fastAPI alternative

---
+++ lists/lists1.py
@@ -0,0 +1,14 @@
+"""friends = ["Kevin", "Karen", "Jim"]
+anything_works = ["Kevin", 2, True]
+print(friends)
+print(anything_works)
+print(friends[1:])
+print(friends[-1]) """
+
+# List Functions Now
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Tom"]
+
+friends.extend(lucky_numbers)
+
+print (friends)
